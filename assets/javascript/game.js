96ee6f617565c45bdef3ef3bba6d9893799d9d4f
Anibal <guerra15anibal@hotmail.com>
2020-02-23 23:41:30 -0700

    changed a lot of js

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,18 @@
-//vars
-var winCount ="";
-var loseCount="";
-var totalScore= "";
-var numberOptions= [7,10,4,3]
+$(document).ready(function(){
+    
+    var randomNum= Math.floor(Math.random()*100+14);
+
+    $("#newNum").text(randomNum);
+
+    var num1 = Math.floor(Math.random()*12+1)
+    var num2 = Math.floor(Math.random()*12+1)
+    var num3 = Math.floor(Math.random()*12+1)
+    var num4 = Math.floor(Math.random()*12+1)
+
+    var score=0;
+    var wins=0;
+    var losses=0;
+
+    $("numWins").text(wins);
+    $
+})
