464887edc5981b565ecde6f0cbf4a097e7227961
Anibal <guerra15anibal@hotmail.com>
2020-02-26 12:00:42 -0700

    fixing javascript link issue and adding functions

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,18 +1,50 @@
-$(document).ready(function(){
-    
-    var randomNum= Math.floor(Math.random()*100+14);
+// $(document).ready(function () {
 
-    $("#newNum").text(randomNum);
+    var randomNum = Math.floor(Math.random() * 100 + 14);
+console.log(randomNum)
+    $("#RandoNum").text(randomNum);
 
-    var num1 = Math.floor(Math.random()*12+1)
-    var num2 = Math.floor(Math.random()*12+1)
-    var num3 = Math.floor(Math.random()*12+1)
-    var num4 = Math.floor(Math.random()*12+1)
+    var crystalOne = Math.floor(Math.random() * 12 + 1);
+    console.log(crystalOne)
+    var crystalTwo = Math.floor(Math.random() * 12 + 1);
+    console.log(crystalTwo)
+    var crystalThree = Math.floor(Math.random() * 12 + 1);
+    console.log(crystalThree)
+    var crystalFour = Math.floor(Math.random() * 12 + 1);
+    console.log(crystalFour)
 
-    var score=0;
-    var wins=0;
-    var losses=0;
+    var playerScore = 0;
+    var wins = 0;
+    var losses = 0;
 
     $("numWins").text(wins);
-    $
-})
+    $("numLosses").text(losses);
+
+    function newGame () {
+        randomNum = Math.floor(Math.random() * 12 + 1);
+        console.log(randomNum);
+        $("#newNum").text(randomNum);
+        var num1 = Math.floor(Math.random() * 12 + 1);
+        var num2 = Math.floor(Math.random() * 12 + 1);
+        var num3 = Math.floor(Math.random() * 12 + 1);
+        var num4 = Math.floor(Math.random() * 12 + 1);
+        playerScore = 0;
+        $("#Score").text(playerScore);
+    }
+
+    function wins(){
+        wins++
+        $("#win").text(wins);
+        newGame();
+    }
+
+    function losses(){
+        lose++
+        $("#lose").text(losses);
+        newGame();
+    }
+
+    
+
+    
+// })
